3781d9465f2c249dbabab0d23854e688e3063a46
YAT <yat@yat-net.com>
2023-04-12 15:32:16 +0900

    end-of-chart4

--- todo/app/Http/Controllers/TaskController.php
+++ todo/app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 
 use Illuminate\Http\Request;
 
@@ -10,11 +11,21 @@ class TaskController extends Controller
 {
     public function index( int $id )
     {
+        //すべてのフォルダを取得
         $folders = Folder::all();
+
+        //選択されたフォルダを取得
+        $current_folder = Folder::find( $id );
+
+        //選択されたフォルダに紐づくタスクを取得
+        // $tasks = Task::where( 'folder_id', $current_folder->id ) -> get();
+        $tasks = $current_folder -> tasks() -> get();
+
 
         return view( 'tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ] );
     }
 }
--- todo/app/Models/Folder.php
+++ todo/app/Models/Folder.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Models;
-// namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    use HasFactory;
+    public function tasks()
+    {
+        return $this->hasMany( 'App\Models\Task' )
+    }
 }
